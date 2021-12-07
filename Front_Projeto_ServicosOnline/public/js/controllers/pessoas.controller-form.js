5b5d04ec5c01e132b0fa062e55475446da6f5cbb
juliana.mattiello <juli.gmattiello@gmail.com>
2021-12-06 22:37:17 -0300

    Atualizacao

--- Front_Projeto_ServicosOnline/public/js/controllers/pessoas.controller-form.js
+++ Front_Projeto_ServicosOnline/public/js/controllers/pessoas.controller-form.js
@@ -59,17 +59,45 @@
                 $timeout(function(){
                     PessoaService.save(vm.pessoa).success(function () {
                         $location.path("/pessoa");
-                        alert("Contribuinte cadastrado com sucesso!!");
+                        message('sucesso', 'Contribuinte cadastrado com sucesso!');
                     }).error(function (erro) {
-                        alert(JSON.stringify(erro));
+                        let msg = erro;
+                        if(msg.erro){
+                            message('erro',msg.erro);
+                        }else{
+                            message('erro',JSON.stringify(erro));
+                        }
                     });
                 },10)
-            
-
-        }
+       }
 
     function select(valor){
         return '"'+valor+'"';
+    }
+
+    function message(tipo, mensagem) {
+        let text = "";
+        if (tipo === 'info') {
+            tipo = 'alert alert-info';
+            text = 'Informação!';
+        }
+        if (tipo === 'sucesso') {
+            tipo = 'alert alert-success';
+            text = 'Sucesso!';
+        }
+        if (tipo === 'erro') {
+            tipo = 'alert alert-danger';
+            text = 'Erro!';
+        }
+        let message = '<div id="alerta" class="' + tipo + '" id="bsalert">';
+        message += '    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> ';
+        message += '    <strong>' + text + '</strong> ' + mensagem + '  ';
+        message += ' </div> ';
+        $("#divPrincipal").append(message);
+
+        setTimeout(function(){ 
+            $("#alerta").alert('close');
+         }, 3000);
     }
 
     }
