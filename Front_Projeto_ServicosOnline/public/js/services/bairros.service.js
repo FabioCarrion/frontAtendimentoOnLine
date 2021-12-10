17d8538f47cbe607531dae4e60cb1a2756538c8e
FabioCarrion <85795012+FabioCarrion@users.noreply.github.com>
2021-12-10 09:00:52 -0300

    Update bairros.service.js

--- Front_Projeto_ServicosOnline/public/js/services/bairros.service.js
+++ Front_Projeto_ServicosOnline/public/js/services/bairros.service.js
@@ -15,7 +15,7 @@
             remove: remove
         };
 
-        var URL = 'http://localhost:3000/api/bairros';
+        var URL = 'https://servicoonline.herokuapp.com/api/bairros';
 
         return service;
 
